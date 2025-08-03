6ac6ca4adeab114ba96ad4ba32e1c7a4413f6f9d
attisch82 <attila.farkas.sd@gmail.com>
2025-08-03 16:46:23 +0200

    Creates library convention plugin

--- build-logic/src/main/java/com/atesz/build_logic/AndroidLibraryConventionPlugin.kt
+++ build-logic/src/main/java/com/atesz/build_logic/AndroidLibraryConventionPlugin.kt
@@ -1,4 +1,23 @@
 package com.atesz.build_logic
 
-class AndroidLibraryConventionPlugin {
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class AndroidLibraryConventionPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+                apply("com.atesz.android.common")
+            }
+            extensions.configure<LibraryExtension> {
+                defaultConfig {
+                    consumerProguardFiles("consumer-rules.pro")
+                }
+            }
+        }
+    }
 }
